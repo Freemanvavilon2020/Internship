5ade58c2fa86035f395c82b0fd8a1378791964eb
Freemanvavilon2020 <Freeman-crew@mail.ru>
2021-12-19 07:51:28 +0300

    Правки

--- 44_day/todo/app/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['email']);
-session_destroy();
-header("location: /login.php");
--- 44_day/todo/app/templates/footer.php
+++
@@ -1,4 +0,0 @@
-        <script src="js/jquery-3.2.1.min.js"></script>
-        <script src="../public/bootstrap-5.1.3-dist/js/bootstrap.bundle.js"></script>
-    </body>
-</html>
