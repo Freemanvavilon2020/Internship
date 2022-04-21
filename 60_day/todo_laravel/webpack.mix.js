dd38b4bd12d242880df2acb9590465c63bacaa46
Freemanvavilon2020 <Freeman-crew@mail.ru>
2022-04-22 01:43:31 +0300

    Правки

--- 60_day/todo_laravel/webpack.mix.js
+++ 60_day/todo_laravel/webpack.mix.js
@@ -3,8 +3,6 @@ const mix = require('laravel-mix');
 /*
  |--------------------------------------------------------------------------
  |
- |--------------------------------------------------------------------------
- |
  | Mix provides a clean, fluent API for defining some Webpack build steps
  | for your Laravel applications. By default, we are compiling the CSS
  | file for the application as well as bundling up all the JS files.
